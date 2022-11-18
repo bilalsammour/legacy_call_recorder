1c067a8964838a0ebdc659bb5327f1bc8b6bd21f
Bilal Sammour <bilalasmor@gmail.com>
2022-11-18 10:44:27 +0300

    changing namespace

--- app/src/main/java/com/threebanders/recordrs/common/Constants.kt
+++
@@ -1,7 +0,0 @@
-package com.threebanders.recordrs.common
-
-class Constants {
-    companion object {
-        const val APP_NAME = "RECORD"
-    }
-}
--- app/src/main/java/com/threebanders/recordrs/common/DialogInfo.kt
+++
@@ -1,3 +0,0 @@
-package com.threebanders.recordrs.common
-
-class DialogInfo(var title: Int, var message: Int, var icon: Int)
