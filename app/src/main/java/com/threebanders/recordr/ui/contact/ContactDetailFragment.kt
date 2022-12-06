aa63cad640512dfc37de1a83a47aa0a9ae8c1151
Bilal Sammour <bilalasmor@gmail.com>
2022-12-06 13:15:16 +0300

    fix

--- app/src/main/java/com/threebanders/recordr/ui/contact/ContactDetailFragment.kt
+++ app/src/main/java/com/threebanders/recordr/ui/contact/ContactDetailFragment.kt
@@ -41,7 +41,7 @@ open class ContactDetailFragment : BaseFragment() {
     protected var recordingsRecycler: RecyclerView? = null
     private var detailView: RelativeLayout? = null
     protected var selectMode = false
-    private lateinit var mainViewModel: MainViewModel
+    lateinit var mainViewModel: MainViewModel
     protected var selectedItems: MutableList<Int>? = ArrayList()
 
     private var selectedItemsDeleted = 0
